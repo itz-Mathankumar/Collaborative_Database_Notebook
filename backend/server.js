1334ea9e35f157d0be9164eefd57c4c016d6a4f4
itz-Mathankumar <mathankumarv.dev@gmail.com>
2024-09-27 07:28:49 +0530

    feat: ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const dotenv = require('dotenv').config();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,6 +13,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.use(express.static('build'));
 
 let db;
 
