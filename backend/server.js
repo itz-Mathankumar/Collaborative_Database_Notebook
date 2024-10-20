ec4606ff66f5226588bbb62f9dcc9f2b8214dc59
itz-Mathankumar <mathankumarv.dev@gmail.com>
2024-10-20 23:33:21 +0530

    modify get request

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
